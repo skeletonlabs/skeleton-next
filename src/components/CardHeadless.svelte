86a9769e4853c222ec6f698bd6bdc2c1bfa9aace
endigo9740 <gundamx9740@gmail.com>
2023-10-14 11:32:35 -0500

    True headless component example added

---
+++ src/components/CardHeadless.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+  // Props (settings)
+  export let href: string;
+  export let title: string;
+  export let body: string;
+
+  // Props (styles)
+  export let elemBase = "";
+  export let elemHeading = "";
+  export let elemParagraph = "";
+</script>
+
+<a {href} class={elemBase}>
+  <h2 class={elemHeading}>{@html title}</h2>
+  <p class={elemParagraph}>{@html body}</p>
+</a>

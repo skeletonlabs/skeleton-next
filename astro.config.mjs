31aedfbb8cdd7f56a5685d3f3419fc11801fcf10
endigo9740 <gundamx9740@gmail.com>
2023-12-28 12:20:52 -0600

    Refactor React card to multi comps

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,5 +6,5 @@ import svelte from "@astrojs/svelte";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react(), svelte()]
+  integrations: [tailwind(), react({ experimentalReactChildren: true }), svelte()]
 });
--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,66 +1,72 @@
 import React from "react";
 
-interface CardProps {
+interface CardProps extends React.PropsWithChildren {
   href: string;
   // Root
   rootBase?: string;
   rootBg?: string;
   rootSpaceY?: string;
   rootRest?: string;
-  // Title
-  titleBase?: string;
-  titleRest?: string;
-  // Article
-  articleBase?: string;
-  articleRest?: string;
-  // Footer
-  footerBase?: string;
-  footerRest?: string;
-  // Slots
-  // https://sandroroth.com/blog/react-slots/
-  title?: string;
-  article?: string;
-  footer?: string;
 }
 
-const Card: React.FC<CardProps> = ({
+interface CardTitleProps extends React.PropsWithChildren {
+  title?: string;
+  titleBase?: string;
+  titleRest?: string;
+}
+
+interface CardArticleProps extends React.PropsWithChildren {
+  article?: string;
+  articleBase?: string;
+  articleRest?: string;
+}
+
+interface CardFooterProps extends React.PropsWithChildren {
+  footer?: string;
+  footerBase?: string;
+  footerRest?: string;
+}
+
+export const CardTitle: React.FC<CardTitleProps> = ({
+  titleBase = "h2",
+  titleRest = "",
+  children = "",
+}): React.ReactElement => {
+  return <h2 className={`${titleBase} ${titleRest}`}>{children}</h2>;
+};
+
+export const CardArticle: React.FC<CardArticleProps> = ({
+  articleBase = "",
+  articleRest = "",
+  children,
+}): React.ReactElement => {
+  return (
+    <article className={`${articleBase} ${articleRest}`}>{children}</article>
+  );
+};
+
+export const CardFooter: React.FC<CardFooterProps> = ({
+  footerBase = "border-t border-white/10 pt-2",
+  footerRest = "",
+  children,
+}): React.ReactElement => {
+  return <footer className={`${footerBase} ${footerRest}`}>{children}</footer>;
+};
+
+export const Card: React.FC<CardProps> = ({
   href,
-  // Root
   rootBase = "card",
   rootBg = "bg-slate-800",
   rootSpaceY = "space-y-2",
-  rootRest,
-  // Title
-  titleBase = "h2",
-  titleRest,
-  // Article
-  articleBase,
-  articleRest,
-  // Footer
-  footerBase = "border-t border-white/10 pt-2",
-  footerRest,
-  // Slots
-  title,
-  article,
-  footer,
+  rootRest = "",
+  children = "",
 }): React.ReactElement => {
   return (
     <a
       href={href}
       className={`${rootBase} ${rootBg} ${rootSpaceY} ${rootRest}`}
     >
-      {/* Title */}
-      {title && <h2 className={`${titleBase} ${titleRest}`}>{title}</h2>}
-      {/* Article */}
-      {article && (
-        <article className={`${articleBase} ${articleRest}`}>{article}</article>
-      )}
-      {/* Footer */}
-      {footer && (
-        <footer className={`${footerBase} ${footerRest}`}>{footer}</footer>
-      )}
+      {children}
     </a>
   );
 };
-
-export default Card;

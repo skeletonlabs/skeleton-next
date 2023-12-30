cec57b910fca82a8a4022ac3b6fd2a8495a9fb18
hugos68 <hugokorteapple@gmail.com>
2023-12-31 00:36:58 +0100

    Massively reduced code thanks to Object.assign :facepalm: (thanks coka)

--- src/lib/react/compose.ts
+++ src/lib/react/compose.ts
@@ -2,9 +2,5 @@ export function composeComponents<T extends React.FC, U extends Object>(
 	root: T,
 	components: U,
 ) {
-	const result = root as T & { [K in keyof U]: U[K] };
-	for (const [key, component] of Object.entries(components)) {
-		result[key as keyof U] = component;
-	}
-	return result;
+	return Object.assign(root, components);
 }

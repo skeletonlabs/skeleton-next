591f9bf6866744030a844b7dd073f9e236f7eb46
hugos68 <hugokorteapple@gmail.com>
2023-12-31 17:11:02 +0100

    Improved type safety to only allow  react fc components

--- src/lib/react/compose.ts
+++ src/lib/react/compose.ts
@@ -1,6 +1,6 @@
-export function composeComponents<T extends React.FC, U extends Object>(
-	root: T,
-	components: U,
-) {
+export function composeComponents<
+	T extends React.FC,
+	U extends Record<string, unknown>,
+>(root: T, components: { [k in keyof U]: React.FC<U[k]> }) {
 	return Object.assign(root, components);
 }

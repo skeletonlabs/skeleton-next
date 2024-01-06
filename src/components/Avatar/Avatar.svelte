e5df2cbe70930b2b9587eaef7854126c3c91905f
lwnzim3 <mahmoud.zino@liebherr.com>
2024-01-06 23:59:27 +0100

    applied some of the review suggestions+

--- src/components/Avatar/Avatar.svelte
+++ src/components/Avatar/Avatar.svelte
@@ -1,85 +1,97 @@
 <script lang="ts">
-    import type { Action } from "svelte/action";
-    
-    // Note: consider `./types.ts`
-    interface AvatarProps {
-        // initials
-        initials?: string;
-        initialsFill?: string;
-        initialsFontSize?: number;
-        initialsBase?: string;
-        initialsRest?: string;
+    import type { Action } from 'svelte/action';
 
-        // image
-        imageBase?:string;
+    interface AvatarProps {
+        href?: string;
+        action?: Action<HTMLElement, string>;
+        actionParams?: string;
+
+        // Root (figure)
+        rootBase?: string;
+        rootBg?: string;
+        rootText?: string;
+        rootFontWeight?: string;
+        rootWidth?: string;
+        rootBorder?: string;
+        rootRounded?: string;
+        rootShadow?: string;
+        rootRest?: string;
+
+        // Image
         imageSrc?: string;
         imageAlt?: string;
-        imageFallback?: string;
-        imageAction?: Action<HTMLElement, string>;
-        imageActionParams?: string;
+        imageBase?: string;
         imageRest?: string;
 
-        // figure
-        figureBase?: string;
-        figureBackground?: string;
-        figureWidth?: string;
-        figureBorder?: string;
-        figureRounded?: string;
-        figureShadow?: string;
-        figureCursor?: string;
-        figureRest?: string;
+        // Initials
+        initialsText?: string;
+        initialsBase?: string;
+        initialsFill?: string;
+        initialsFontSize?: number;
+        initialsRest?: string;
     }
 
     let {
-        // initials
-        initials = "AB",
-        initialsFill = "fill-token",
-        initialsFontSize = 150,
-        initialsBase = "w-full h-full",
-        initialsRest = "",
+        href = "",
+        action = () => {},
+        actionParams = "",
 
-        //image
+        // Root (figure)
+        rootBase = "overflow-hidden isolate",
+        rootBg = "bg-surface-400-500-token",
+        rootWidth = "w-16",
+        rootBorder = "",
+        rootRounded = "rounded-full",
+        rootShadow = "",
+        rootRest = "",
+
+        // Image
         imageBase = "w-full object-cover",
         imageSrc = "",
         imageAlt = "",
-        imageFallback = "",
-        imageAction = () => {},
-        imageActionParams = "",
         imageRest = "",
 
-        // figure
-        figureBase = "flex aspect-square text-surface-50 font-semibold justify-center items-center overflow-hidden isolate",
-        figureBackground = "bg-surface-400-500-token",
-        figureWidth = "w-16",
-        figureBorder = "",
-        figureRounded = "rounded-full",
-        figureShadow = "",
-        figureCursor = "",
-        figureRest = "",
+        // Initials
+        initialsText = "AB",
+        initialsBase = "w-full h-full",
+        initialsFill = "fill-token",
+        initialsFontSize = 150,
+        initialsRest = "",
     } = $props<AvatarProps>();
 </script>
 
-<figure class="{figureBase} {figureBackground} {figureWidth} {figureBorder} {figureRounded} {figureShadow} {figureCursor} {figureRest}">
-    {#if imageSrc}
-        <img 
-            class="{imageBase} {imageRest}" 
-            src={imageSrc} 
-            alt={imageAlt}
-            use:imageAction={imageActionParams} 
-            onerror={() => imageSrc = imageFallback}
-        />
-    {:else}
-        <svg class="{initialsBase} {initialsRest}" viewBox="0 0 512 512">
-            <text
-                x="50%"
-                y="50%"
-                dominant-baseline="central"
-				text-anchor="middle"
-				font-weight="bold"
-                font-size={initialsFontSize}
-                class="{initialsFill}">
-                {String(initials).substring(0, 2).toUpperCase()}
-            </text>
-        </svg>
-    {/if}
-</figure>
+{#snippet figure()}
+    <figure class="{rootBase} {rootBg} {rootWidth} {rootBorder} {rootRounded} {rootShadow} {rootRest}">
+        {#if imageSrc}
+            <!-- TODO: test moving actions to figure. -->
+            <img
+                class="{imageBase} {imageRest}" 
+                src={imageSrc} 
+                alt={imageAlt}
+                use:action={actionParams}
+            />
+        {:else}
+            <!-- TODO: investigate a better approach using Tailwind. -->
+            <svg class="{initialsBase} {initialsRest}" viewBox="0 0 512 512">
+                <text
+                    x="50%"
+                    y="50%"
+                    dominant-baseline="central"
+                    text-anchor="middle"
+                    font-weight="bold"
+                    font-size={initialsFontSize}
+                    class="{initialsFill}">
+                    {String(initialsText).substring(0, 2).toUpperCase()}
+                </text>
+            </svg>
+        {/if}
+    </figure>
+{/snippet}
+
+{#if href}
+    <a {href} class="{rootRounded}">
+        {@render figure()}
+    </a>
+{:else}
+    {@render figure()}
+{/if}
--- src/pages/components/avatars/PreviewSvelte.svelte
+++ src/pages/components/avatars/PreviewSvelte.svelte
@@ -3,5 +3,10 @@
 
 </script>
 
-<Avatar imageSrc="https://images.unsplash.com/photo-1617296538902-887900d9b592?ixid=M3w0Njc5ODF8MHwxfGFsbHx8fHx8fHx8fDE2ODc5NzExMDB8&ixlib=rb-4.0.3&w=128&h=128&auto=format&fit=crop" 
-    figureWidth="w-32" figureRounded="rounded-full"/>
+<div class="flex space-x-4">
+
+    <Avatar imageSrc="https://images.unsplash.com/photo-1617296538902-887900d9b592?ixid=M3w0Njc5ODF8MHwxfGFsbHx8fHx8fHx8fDE2ODc5NzExMDB8&ixlib=rb-4.0.3&w=128&h=128&auto=format&fit=crop" 
+        rootWidth="w-32"/>
+    
+    <Avatar rootWidth="w-32" rootBg="bg-yellow-500" href="#"/>
+</div>

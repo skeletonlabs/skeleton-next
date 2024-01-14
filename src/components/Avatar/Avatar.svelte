08e91a6e47b423aa62007b02f04a6ed9f9e41e84
Mahmoud-zino <mahmoud.alhalaby@gmail.cim>
2024-01-14 20:21:02 +0100

    applied code-review suggestions

--- src/components/Avatar/Avatar.svelte
+++ src/components/Avatar/Avatar.svelte
@@ -11,7 +11,7 @@
         base?: string;
         background?: string;
         text?: string;
-        fontWeight?: string;
+        font?: string;
         width?: string;
         border?: string;
         rounded?: string;
@@ -37,6 +37,7 @@
         base = "overflow-hidden isolate",
         background = "bg-surface-400-500-token",
         width = "w-16",
+        font = "",
         border = "",
         rounded = "rounded-full",
         shadow = "",
@@ -54,31 +55,15 @@
 </script>
 
 {#snippet figure()}
-    <figure class="{base} {background} {width} {border} {rounded} {shadow} {classes}">
-        {#if children}
-        <!-- TODO: when Snippet types are implemented in svelte-5 check for type and if string provide an SVG with the text/initials -->
-            {@render children()}
-            <!-- TODO: investigate a better approach using Tailwind. -->
-            <!-- <svg class="{initialsBase} {initialsRest}" viewBox="0 0 512 512">
-                <text
-                    x="50%"
-                    y="50%"
-                    dominant-baseline="central"
-                    text-anchor="middle"
-                    font-weight="bold"
-                    font-size={initialsFontSize}
-                    class="{initialsFill}">
-                    {String(initialsText).substring(0, 2).toUpperCase()}
-                </text>
-            </svg> -->
-        {:else}
-            <!-- TODO: test moving actions to figure. -->
+    <figure class="{base}{background} {width} {font} {border} {rounded} {shadow} {classes}" use:action={actionParams}>
+        {#if imageSrc}
             <img
                 class="{imageBase} {imageRest}" 
                 src={imageSrc} 
                 alt={imageAlt}
-                use:action={actionParams}
             />
+        {:else if children}
+            {@render children()}
         {/if}
     </figure>
 {/snippet}
